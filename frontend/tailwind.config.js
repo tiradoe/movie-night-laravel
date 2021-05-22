e3f804b314129792004930646adeed41254ec45f
Edward Tirado Jr <ed@etdevelopment.net>
2021-05-22 01:17:33 -0600

    updated packages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.ts"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-        backgroundColor: (theme) => ({
-            ...theme("colors"),
-            header: "#016379",
-            button: "rgba(59, 130, 246, var(--tw-bg-opacity))",
-        }),
+  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.ts"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      header: "#016379",
+      button: "rgba(59, 130, 246, var(--tw-bg-opacity))",
+    }),
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["even"],
     },
-    variants: {
-        extend: {
-            backgroundColor: ["even"],
-        },
-    },
-    plugins: [],
+  },
+  plugins: [],
 };
