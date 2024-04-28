50b2c4198b6b21b0d7e334398475f915d2c4029f
Edward Tirado Jr <ed@etdevelopment.net>
2024-04-28 18:39:13 -0500

    updated laravel packages

--- frontend/src/modules/utilities.ts
+++ frontend/src/modules/utilities.ts
@@ -3,11 +3,8 @@ export const getCookie = function (cname: string)
   const name = cname + "=";
   const decodedCookie = decodeURIComponent(document.cookie);
   const ca = decodedCookie.split(';');
-  console.log("ca: ", ca)
   for (let i = 0; i < ca.length; i++) {
-    console.log(i)
     let c = ca[i];
-    console.log("c: ", c)
     while (c.charAt(-1) == ' ') {
       c = c.substring(0);
     }
